9a1c3ace4593e62d65ed966edb77c6ce7e51be62
Guilherme Ytalo <gydaytalo@gmail.com>
2023-03-05 17:44:44 -0300

    update issues on readme

--- hello_world/lib/hello_world.ex
+++ hello_world/lib/hello_world.ex
@@ -1,18 +1,5 @@
 defmodule HelloWorld do # module name
   @message "Hello,new world"
-  @moduledoc """
-  Documentation for `HelloWorld`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      # iex> HelloWorld.hello()
-      # :world
-
-  """
   # function name with/whithout params
   def hello do
     # :world
